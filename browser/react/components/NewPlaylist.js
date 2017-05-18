44e44899057b9ef66685d023e3a16eb224df9e91
JHFellows <j.fellows88@gmail.com>
2017-05-18 13:01:40 -0400

    day 2

--- browser/react/components/NewPlaylist.js
+++ browser/react/components/NewPlaylist.js
@@ -2,21 +2,27 @@ import React from 'react';
 import { Link } from 'react-router';
 
 const NewPlaylist = (props) => {
-  console.log(props)
+  console.log(props.tooLong, props.tooShort)
   return (
 <div className="well">
-  <form className="form-horizontal">
+  <form className="form-horizontal" onSubmit={props.handleSubmit}>
     <fieldset>
       <legend>New Playlist</legend>
       <div className="form-group">
         <label className="col-xs-2 control-label">Name</label>
         <div className="col-xs-10">
-          <input className="form-control" type="text"/>
+          <input type="text" value={props.inputValue} onChange={props.handleChange} className="form-control"  />
         </div>
       </div>
       <div className="form-group">
         <div className="col-xs-10 col-xs-offset-2">
-          <button type="submit" className="btn btn-success">Create Playlist</button>
+        {props.show && props.tooLong ? <div className="alert alert-warning">Your playlist name is too long</div> : null}
+        {props.show && props.tooShort ? <div className="alert alert-warning">Please enter a name</div> : null}
+          <button
+          disabled={props.inputValue === "" || props.inputValue.length > 16 ? true : false}
+          type="submit" className="btn btn-success">
+          Create Playlist
+          </button>
         </div>
       </div>
     </fieldset>
--- browser/react/containers/PlaylistContainer.js
+++ browser/react/containers/PlaylistContainer.js
@@ -16,32 +16,57 @@ export default class PlaylistContainer extends Component {
   constructor(props){
     super(props);
     this.state = {
-      inputValue: ''
+      inputValue: '',
+      show: false,
+      tooLong: false,
+      tooShort: false
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
+  componentDidMount{
+  axios.post('/api/playlists', this.state.inputValue)
+    .then(res => res.data)
+    .then(result => {
+      console.log(result) // response json from the server!
+    });
+  }
+
   handleChange (event){
     const value = event.target.value;
     this.setState({
       inputValue: value
     })
+    if(this.state.inputValue === ''){
+      this.setState({
+        show: true,
+        tooShort: true,
+        tooLong: false
+      })
+    }else if(this.state.inputValue.length > 16){
+      this.setState({
+        show: true,
+        tooShort: false,
+        tooLong: true
+      })
+    } else{
+      this.setState({
+        show:false
+      })
+    }
   }
+
   handleSubmit(event){
-    console.log("NEW PLAYLIST?" + this.state.value);
+    console.log("NEW PLAYLIST?" + this.state.inputValue);
+    this.setState({
+      inputValue: ""
+    })
     event.preventDefault();
   }
 
   render(){
-    <NewPlaylist />
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Playlist Name:
-          <input type="text" value={this.state.inputValue} onChange={this.handleChange}/>
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+      <NewPlaylist handleSubmit={this.handleSubmit} handleChange={this.handleChange} inputValue={this.state.inputValue} show={this.state.show} tooShort={this.state.tooShort} tooLong={this.state.tooLong}/>
     );
   }
 
